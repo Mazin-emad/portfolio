c117d7baaf6ba92f62f06dbb4e10f9ab4720855c
Mazin Emad <124470200+Mazin-emad@users.noreply.github.com>
2025-06-11 04:25:39 +0300

    fixed deploement error

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 "use client";
 
-import Navbar from "@/components/Navbar";
-import Header from "@/components/Header";
-import About from "@/components/About";
-import Services from "@/components/Services";
-import Portfolio from "@/components/Portfolio";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+import About from "../components/About";
+import Services from "../components/Services";
+import Portfolio from "../components/Portfolio";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
